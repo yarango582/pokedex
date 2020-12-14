3a6992431fd9042a70f9dd14be29ed949ebae31e
YarangoDev <yeisonarango03@gmail.com>
2020-12-13 23:12:04 -0500

    mejora de estilos

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,16 +6,16 @@ function Navbar (props){
     return(
         <div className="navbar">
             <nav className="navigation">
-                <h3 className="title">Pokedex</h3>
                 <img 
-                className="logo-p"
-                src="https://www.flaticon.es/svg/static/icons/svg/188/188918.svg" 
-                alt="logo"
-                />
-            </nav>
+                    className="logo-p"
+                    src="https://www.flaticon.es/svg/static/icons/svg/188/188918.svg" 
+                    alt="logo"
+                    />  
             <form className="form-search-bar">
                 <input type="text" name="search" placeholder="Search Pokemon" className="search-bar" onChange={props.handleInput}/>
             </form>
+            </nav>
+            <h3 className="title">Pokedex</h3>
         </div>
     )
 
